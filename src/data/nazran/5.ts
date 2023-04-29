4c9a97e17211502041a098d65d1ea4f684e724c6
aziev <mikail.aziev@gmail.com>
2023-04-29 17:48:13 +0300

    add start of May schedule

---
+++ src/data/nazran/5.ts
@@ -0,0 +1,34 @@
+import { DailySchedule } from '../../types'
+
+export const schedule: DailySchedule[] = [
+  {
+    day: 1,
+    fajr: '03:16',
+    sunset: '04:55',
+    zuhr: '12:04',
+    asr: '15:53',
+    maghrib: '19:03',
+    isha: '20:34',
+  },
+  {
+    day: 2,
+    fajr: '03:14',
+    sunset: '04:53',
+    zuhr: '12:04',
+    asr: '15:53',
+    maghrib: '19:05',
+    isha: '20:36',
+  },
+  {
+    day: 3,
+    fajr: '03:12',
+    sunset: '04:52',
+    zuhr: '12:04',
+    asr: '15:54',
+    maghrib: '19:06',
+    isha: '20:37',
+  }
+]
+
+
+
